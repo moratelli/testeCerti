5a9a57b5a38e0ba58d2bc410944c5dca297e3e8f
Pedro <pedromoratelli@gmail.com>
2020-02-15 13:43:38 -0300

    Criar Middleware de verificação de request
    O middleware verifica se a requisição é um número e se está dentro do range -99999 e 99999. Caso alguma dessas verificações falhe, o middleware retorna um erro 400 e um JSON descrevendo a falha.

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const express = require("express");
 const server = express();
 
+server.use(express.json());
+
+/* MIDDLEWARE */
+const checkNumber = (req, res, callback) => {
+  const data = req.params.number;
+  console.log(data);
+
+  if (isNaN(data) === true)
+    return res.status(400).json({ error: "Not a number!" });
+  else if (data <= -100000)
+    return res.status(400).json({ error: "Number is under -99999!" });
+  else if (data >= 100000)
+    return res.status(400).json({ error: "Number is over 99999!" });
+
+  return callback();
+};
+
 /* ROUTES */
-server.get("/", (req, res) => {
-  return res.status(200).json({ Hello: "World" });
+server.get("/:number", checkNumber, (req, res) => {
+  const number = req.params.number;
+
+  return res.status(200).json({ All: "Good!" });
 });
 
 server.listen(3000);
